6b77f2f8506a81e92737358ba51e1e0809a338d0
CodeCraftKiran <codecraftkiran@gmail.com>
2024-05-19 16:34:04 +0530

    travel-tracker complete

--- 8.3+Travel+Tracker/index.js
+++ 8.3+Travel+Tracker/index.js
@@ -18,10 +18,52 @@ const db = new pg.Client({
 
 db.connect();
 
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((row) =>{
+    countries.push(row['country_code'])
+  });
+  return countries;
+}
+
 app.get("/", async (req, res) => {
-  const countries = await db.query("SELECT country_code FROM visited_countries");
+  const countries = await checkVisisted();
   res.render('index.ejs', {countries: countries, total: countries.length})
 });
+
+
+app.post("/add", async( req, res) => {
+  const country_name = req.body.country;
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) like '%' || $1 || '%'",
+      [country_name.toLowerCase()]);
+    const country_code = result.rows[0]['country_code'];
+
+    try {
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [country_code])
+    res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
+    }
+    
+} catch (err){
+  const countries = await checkVisisted();
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    error: "Country does not exist! Please try again!"
+  })
+}
+})
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
